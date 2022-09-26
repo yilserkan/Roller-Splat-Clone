4d9b71d8e5632781ad9a6daba3f856ab912b3ae5
Serkan <yilserkan123@gmail.com>
2022-09-26 11:41:14 +0300

    created Abstact Singelton

---
+++ Roller Splat Clone/Assets/Scripts/Utils/AbstractSingelton.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils
+{
+    public abstract class AbstractSingelton<T> : MonoBehaviour where T : Component
+    {
+        private static T m_Instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                {
+                    T[] foundObjects = FindObjectsOfType<T>();
+                    if (foundObjects.Length > 0)
+                    {
+                        m_Instance = foundObjects[0];
+
+                        if (foundObjects.Length > 1)
+                        {
+                            Debug.Log($"There are multiple Singletons of type {nameof(T)}");
+                        }
+                    }
+
+
+                    if (m_Instance == null)
+                    {
+                        var ob = new GameObject();
+                        ob.name = nameof(T);
+
+                        var singelton = ob.AddComponent<T>();
+                        m_Instance = singelton;
+                    }
+                }
+
+                return m_Instance;
+            }
+        }
+    }
+}
