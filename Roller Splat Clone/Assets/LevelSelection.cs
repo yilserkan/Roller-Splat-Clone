efb085dc8ad463c523d3e4c165ebc6247f84ef31
Serkan <yilserkan123@gmail.com>
2022-09-29 22:09:08 +0300

    added level selection close animation

---
+++ Roller Splat Clone/Assets/LevelSelection.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MainMenu
+{
+
+    public class LevelSelection : MonoBehaviour
+    {
+        [SerializeField] private Animation animation;
+        
+        private const string m_LevelClose = "Animation_CloseLevelSelect";
+        
+        public void _CloseLevelSelection()
+        {
+            animation.Play(m_LevelClose);
+        }
+
+        public void _DisableLevelSelectionGameobject()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
