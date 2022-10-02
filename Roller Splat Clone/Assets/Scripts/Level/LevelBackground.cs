96641b096f03d1905500b2ac6a2187c0c75438d5
Serkan <yilserkan123@gmail.com>
2022-10-02 14:10:59 +0300

    fixed background plane size according to camera size

--- Roller Splat Clone/Assets/Scripts/Level/LevelBackground.cs
+++ Roller Splat Clone/Assets/Scripts/Level/LevelBackground.cs
@@ -1,3 +1,4 @@
+using CameraSystem;
 using UnityEngine;
 using Grid = GridSystem.Grid;
 
@@ -6,6 +7,8 @@ namespace LevelSystem
     public class LevelBackground : MonoBehaviour
     {
         [SerializeField] private Material backgroundMaterial;
+
+        private float m_AspectRatio = 9f / 16f;
         
         private void OnEnable()
         {
@@ -17,19 +20,41 @@ namespace LevelSystem
             RemoveListeners();
         }
 
+        private void ChangeXZPositionToCameraPos(Vector3 camPosition)
+        {
+            transform.position = new Vector3(camPosition.x, transform.position.y, camPosition.z);
+        }
+        
+        private void CalculateNewBackgroundSize(Vector3 camPosition)
+        {
+            float cameraHeightInScale = (camPosition.y / 10);
+            float yScale = cameraHeightInScale + (cameraHeightInScale * 25 / 100);
+            float xScale = yScale * m_AspectRatio;
+
+            transform.localScale = new Vector3(xScale * 2, transform.localScale.y, yScale * 2);
+        }
+
         private void HandleOnBackgroundColorSet(Color color)
         {
             backgroundMaterial.color = color;
         }
         
+        private void HandleOnCameraPositionFound(Vector3 camPosition)
+        {
+            ChangeXZPositionToCameraPos(camPosition);
+            CalculateNewBackgroundSize(camPosition);
+        }
+        
         private void AddListeners()
         {
             Grid.OnBackgorundColorSet += HandleOnBackgroundColorSet;
+            CameraHandler.OnCameraPositionFound += HandleOnCameraPositionFound;
         }
-
+        
         private void RemoveListeners()
         {
             Grid.OnBackgorundColorSet -= HandleOnBackgroundColorSet;
+            CameraHandler.OnCameraPositionFound -= HandleOnCameraPositionFound;
         }
     }
 }
